552a70019077bf6bd261d8b6b86ff60ee6a3bfa0
n1ghtshade <scs.bon7@gmail.com>
2021-10-08 20:35:15 +0300

    fuctorial

---
+++ src/daaaaa.java
@@ -0,0 +1,19 @@
+public class daaaaa {
+    public static void main(String[] args) {
+        System.out.println(fuct(17));
+    }
+
+
+    public static long fuct(int n){
+        if (n <=1){
+            return 1;
+        }
+
+        long res = n*fuct(n-1);
+        System.out.println(res);
+        return res;
+
+    }
+
+    }
+
